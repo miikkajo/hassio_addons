a6cef730ec487da371401ec2f21e3c0244d8ffba
Miikka Joutsenvirta <miikkajo@hell.djupvik.dy.fi>
2019-10-08 00:34:48 +0300

    disable aa

--- gmusicproxy/run.sh
+++ gmusicproxy/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bashio
 set -e
 bashio::log.info "Starting gmusicproxy..."
-/GMusicProxy --host $(bashio::config 'url') -P $(bashio::config 'port') $(bashio::config 'extra_parameters')
+/GMusicProxy -a --host $(bashio::config 'url') -P $(bashio::config 'port')
