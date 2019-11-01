7831563fe11c58937ecf99bfdd026cdc200e8d35
Miikka Joutsenvirta <miikkajo@hell.djupvik.dy.fi>
2019-11-01 23:34:44 +0200

    +1

--- gmusicproxy/run.sh
+++ gmusicproxy/run.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bashio
 set -e
 bashio::log.info "Starting gmusicproxy..."
-info
-#!/usr/bin/bashio
 ingress_enabled=$(bashio::addon.ingress)
 if [[ $ingress_enabled == "true" ]]; then
     /GMusicProxy -a --host "$(bashio::addon.ingress_entry)" -P $(bashio::config 'port')
