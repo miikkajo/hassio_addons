9d7fe3e98885787538939271bd738b4e3e238c70
Miikka Joutsenvirta <miikkajo@hell.djupvik.dy.fi>
2019-12-26 20:17:07 +0200

    enable all access

--- gmusicproxy/run.sh
+++ gmusicproxy/run.sh
@@ -4,11 +4,7 @@ cp /gui_template.html /data/gui_template.html
 bashio::log.info "Starting gmusicproxy..."
 ingress_enabled=$(bashio::addon.ingress)
 if [[ $ingress_enabled == "true" ]]; then
-    /GMusicProxy -a --host "$(bashio::addon.ingress_entry)" -P $(bashio::config 'port')
+    /GMusicProxy --host "$(bashio::addon.ingress_entry)" -P $(bashio::config 'port')
 else
-    /GMusicProxy -a --host $(bashio::config 'url') -P $(bashio::config 'port')
+    /GMusicProxy --host $(bashio::config 'url') -P $(bashio::config 'port')
 fi
-
-
-
-
