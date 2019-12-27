d497da00ee54ca327e467efdb563897a79de1a00
Miikka Joutsenvirta <miikkajo@hell.djupvik.dy.fi>
2019-12-28 01:07:37 +0200

    modded initial configuration logic

--- gmusicproxy/run.sh
+++ gmusicproxy/run.sh
@@ -3,8 +3,15 @@ set -e
 cp /gui_template.html /data/gui_template.html
 bashio::log.info "Starting gmusicproxy..."
 ingress_enabled=$(bashio::addon.ingress)
-if [[ $ingress_enabled == "true" ]]; then
-    /GMusicProxy --host "$(bashio::addon.ingress_entry)" -P $(bashio::config 'port')
+FILE=/etc/resolv.conf
+if [ ! test -f "/data/mobileclient.cred" ]; then
+    bashio::log.info "No credentials file found, sleeping for 3600 sec, waiting for configure..."
+    sleep 3600
+    bashio::log.info "Not configured exiting..."
 else
+  if [[ $ingress_enabled == "true" ]]; then
+    /GMusicProxy --host "$(bashio::addon.ingress_entry)" -P $(bashio::config 'port')
+  else
     /GMusicProxy --host $(bashio::config 'url') -P $(bashio::config 'port')
+  fi
 fi
