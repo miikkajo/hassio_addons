4c8210c406f47d7f8ea0ecaa33331a90b04019c3
Miikka Joutsenvirta <miikkajo@hell.djupvik.dy.fi>
2019-12-28 01:10:16 +0200

    fix...

--- gmusicproxy/run.sh
+++ gmusicproxy/run.sh
@@ -4,7 +4,7 @@ cp /gui_template.html /data/gui_template.html
 bashio::log.info "Starting gmusicproxy..."
 ingress_enabled=$(bashio::addon.ingress)
 FILE=/etc/resolv.conf
-if [ ! test -f "/data/mobileclient.cred" ]; then
+if [ ! -f "/data/mobileclient.cred" ]; then
     bashio::log.info "No credentials file found, sleeping for 3600 sec, waiting for configure..."
     sleep 3600
     bashio::log.info "Not configured exiting..."
