aae5af24d96db481e08d4051bd1f4fa2a47b4ef0
Miikka Joutsenvirta <miikkajo@iki.fi>
2020-02-22 23:38:19 +0200

    added frigate addon

---
+++ ha-addon-frigate/frigate-beta/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bashio
+
+export FRIGATE_RTSP_PASSWORD=$(bashio::config 'FRIGATE_RTSP_PASSWORD')
+export CONFIG_FILE=$(bashio::config 'CONFIG_FILE')
+sed -i 's|/config/config.yml|'$CONFIG_FILE'|g' /opt/frigate/detect_objects.py 
+python3 -u detect_objects.py
--- refresh_repository.sh
+++ refresh_repository.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 rm -rf gmusicproxy
 rm -rf gmusicproxy-dev
+rm -rf ha-addon-frigate
 git clone https://github.com/miikkajo/gmusicproxy ./gmusicproxy
 rm -rf gmusicproxy/.git 
 git clone --branch dev https://github.com/miikkajo/gmusicproxy gmusicproxy-dev
 rm -rf gmusicproxy-dev/.git 
+git clone https://github.com/miikkajo/ha-addon-frigate ./ha-addon-frigate
+rm -rf ha-addon-frigate/.git 
